e3e0bb508942cfde4281a3d1eea36ec5db171840
denverdino <denverdino@gmail.com>
2025-04-03 20:04:21 +0800

    Sync mcp images

---
+++ generate-mcp-sync-config.py
@@ -0,0 +1,16 @@
+import requests
+
+def list_repos(username):
+    repos = []
+    url = f"https://hub.docker.com/v2/repositories/{username}/?page_size=100"
+    while url:
+        resp = requests.get(url)
+        data = resp.json()
+        repos.extend([repo["name"] for repo in data["results"]])
+        url = data.get("next")
+    return repos
+
+if __name__ == "__main__":
+    username = "mcp"
+    for repo in list_repos(username):
+        print(repo + ":latest: registry.cn-beijing.aliyuncs.com/weiyuan-test/" + repo + ":latest")
