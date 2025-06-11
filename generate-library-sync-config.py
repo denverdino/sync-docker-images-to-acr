1b234e082bc4e6a17c5e2a8d05960fdd775985a6
denverdino <denverdino@gmail.com>
2025-06-11 09:20:21 +0800

    Sync docker library images

---
+++ generate-library-sync-config.py
@@ -0,0 +1,18 @@
+import requests
+
+def list_repos(username):
+    repos = []
+    url = f"https://hub.docker.com/v2/repositories/{username}/?page_size=100"
+    while url:
+        resp = requests.get(url)
+        data = resp.json()
+        repos.extend([repo["name"] for repo in data["results"]])
+        url = data.get("next")
+    return repos
+
+if __name__ == "__main__":
+    username = "library"
+    for repo in list_repos(username):
+        if repo == "signatures":
+            continue
+        print(f"{repo}:latest: registry.cn-beijing.aliyuncs.com/docker-library-mirror/{repo}:latest")
